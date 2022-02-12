80963efb305383e56f474d9d7100cd45aa98d9ac
cryptolake <dhiadah@gmail.com>
2022-02-12 21:58:56 +0100

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,12 @@
 #include "sort.h"
 #include <stdlib.h>
 
+/**
+* swap - swap two integers
+* @ele0: integer 1
+* @ele1: integer 2
+*
+**/
 void swap(int *ele0, int *ele1)
 {
 	int swape;
@@ -13,21 +19,26 @@ void swap(int *ele0, int *ele1)
 }
 
 
+/**
+* bubble_sort - bubble sort algorithm
+* @array: array to sort
+* @size: size of array
+**/
 void bubble_sort(int *array, size_t size)
 {
 	int swapped;
 	size_t i;
+
 	do {
 		swapped = 0;
 		for (i = 1; i < size; i++)
 		{
-			if (array[i-1] > array[i])
+			if (array[i - 1] > array[i])
 			{
-				swap(&array[i-1], &array[i]);
+				swap(&array[i - 1], &array[i]);
 				print_array(array, size);
 				swapped = 1;
 			}
-				
 		}
 
 	} while (swapped);
